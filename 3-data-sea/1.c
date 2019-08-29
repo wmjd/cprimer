a81362afb627d90170f6279152a171f99935e23c
William Diebolt <wmdiebolt@gmail.com>
2019-08-29 13:03:19 -0700

    interesting no float overflow maxf = maxf +1 evidently

--- 3-data-sea/1.c
+++ 3-data-sea/1.c
@@ -8,29 +8,62 @@ int main(void){
 }
 
 void ints(void){
+/* remember 2's-complement,
+		111 -1
+		110 -2
+		101 -3
+		100 -4
+		011  3
+		010  2
+		001  1
+		000  0
+*/
+/*
+	//signed integer
 	int i = 0;
-	while(i+1>0){
+	while(i+1 > 0){
 		i++;
 	}
     printf("i will overflow if inc'd. i=%d i+1=%d\n", i, i+1);
-	//	]=> will overflow if inc'd. i=-2147483648 i+1=2147483647
+	//	]=> will overflow if inc'd. i = 2147483648 i+1= -2147483647
 	
-	i = 1;
-	while(i+1){
-		i++;
-	}
-    printf("i will overflow if inc'd. i=%d i+1=%d\n", i, i+1);
-	//	]=> will overflow if inc'd. i=-1 i+1=0
-
 	i = 0;
-	while(i+1){
-		i++;
+	while((i-1) < 0){
+		i--;
 	}
-    printf("i will overflow if inc'd. i=%d i+1=%d\n", i, i++);
-	// 
-	// think this is because the side effects from i++ (3rd arg to printg) are being evaluated before i (2nr arg)
-	// leaving it so i can remember what happens! 	
+    printf("i will overflow if dec'd. i=%d i-1=%d\n", i, i-1);
+	//	]=> will overflow if inc'd. i= i-1=
+
+	
+
+	//unsigned integer
+	unsigned int u = 0;
+	while (u+1) u++;
+	printf("u: %u u if inc'd: %u\n", u, u+1);	
+	//u: 4294967295 u if inc'd: 0
+*/
+	//float overflow
+
+	float foo = 16777216.000000;	
+	printf("%f %f\n", foo,foo+1);
+	// foo is actually equal to foo+1.
+	//it won't let you overflow, it just stays the same!!
+/*	
+	float f = 0;
+	while (f+1 >= 0) {f++; printf("%f\n",f);}
+	printf("f: %f inc(f): %f", f, f+1);
+*/	
+	
+	//float underflow
+
 }
+
+
+
+
+// clang actually prints a warning, gcc does not.
+// think this is because the side effects from i++ (3rd arg to printg) are being evaluated before i (2nr arg)
+// leaving it so i can remember what happens! 	
 
 
 
---
+++ 3-data-sea/printftypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	unsigned u = -1;
+	signed s= -1;
+
+	printf("u as unsigned is: %u\nu as signed is: %d\ns as unsigned is: %u\ns as signed is %d\n", u,u,s,s);
+}
+
+/*
+u as unsigned is: 4294967295
+u as signed is: -1
+s as unsigned is: 4294967295
+s as signed is -1
+*/
