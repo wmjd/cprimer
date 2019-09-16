4ddbe76e4a4dd6c5c3b66c2232a306fe6aaf0ede
William Diebolt <wmdiebolt@gmail.com>
2019-09-15 21:10:55 -0700

    char arr is str

--- 11-char-strs-str-funs/1.c
+++ 11-char-strs-str-funs/1.c
@@ -1,17 +1,29 @@
-/* fun fetches next n chars frp, input
+/* fun fetches next n chars from input
  * stores in array whose addr is arg
 */
 
 #include <stdio.h>
 
 void f(char *, int);
+void g(char *, int);
 
 int main(void){
+
+	char s[64];
+	int n = 10;
+
+	f(s, n);
+	g(s, n);
 
 	return 0;
 }
 
 void f(char *a, int n){
+	for(int i=0; i<n; i++)
+		a[i] = getchar();	
+}
 
-
+void g(char *a, int n){
+	for(int i=0; i<n; i++)
+		putchar(a[i]);
 }
