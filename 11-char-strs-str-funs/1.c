07544dc8700ab1c43ea733b7dfc41694d5f52add
William Diebolt <wmdiebolt@gmail.com>
2019-09-15 18:13:30 -0700

    starting ch 11

---
+++ 11-char-strs-str-funs/1.c
@@ -0,0 +1,17 @@
+/* fun fetches next n chars frp, input
+ * stores in array whose addr is arg
+*/
+
+#include <stdio.h>
+
+void f(char *, int);
+
+int main(void){
+
+	return 0;
+}
+
+void f(char *a, int n){
+
+
+}
