9190fbc634a98fff25d393a1923ee76fa90b8569
William Diebolt <wmdiebolt@gmail.com>
2019-09-29 23:56:39 -0700

    fixed newline bug

--- 11-char-strs-str-funs/4.c
+++ 11-char-strs-str-funs/4.c
@@ -1,25 +1,28 @@
 /* read first word from line of input into array
  * discard rest of line. skip w/s.
  * a word is a seq of chars w/o w/s
+ * ALSO: take a second parameter specifying max n chars to read
 */
 
 #include <stdio.h>
-#define MAXWORD 80
+#define MAXWORD 10
 
-void readWord(char *);
+void readWord(char *, int);
 int main(void){
 	
 
 	char buf[MAXWORD];
-	readWord(buf);
+	readWord(buf, MAXWORD-1);
 	printf("%s\n", buf);
-	readWord(buf);
+	readWord(buf, MAXWORD-1);
 	printf("%s\n", buf);
 
 	return 0;
 }
 
-void readWord(char * a){
+void readWord(char * a, int n){
+	if(n == 0)
+		return ; //read nothing!
 
 	int c, i=0;
 
@@ -30,12 +33,12 @@ void readWord(char * a){
 	//read word
 	do{
 		a[i++] = c;
-	}while((c=getchar()) != ' ' && c != '\t' && c != '\n');
+	}while(i < n && (c=getchar()) != ' ' && c != '\t' && c != '\n');
 	
 	//add null terminated byte for printing
 	a[i] = 0; 
 
-	//discard trailing ws and nl
-	while(c != '\n')
-		c =	getchar();
+	//discard rest of line
+	while(getchar() != '\n')
+		;
 }
