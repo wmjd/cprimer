ab78949eef11ced93e432504a492c68d4843512f
William Diebolt <wmdiebolt@gmail.com>
2019-10-04 02:38:10 -0700

    user enters some words. double indirection problem

--- 12-storage-classes-linkage-memory/9.c
+++ 12-storage-classes-linkage-memory/9.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAXWORD 1000
 
 int main(void){
 	int nwords;
@@ -11,13 +12,14 @@ int main(void){
 
 	printf("Enter %d words now: ", nwords);
 	for(int i=0; i<nwords; i++){
-		scanf("%s", *words+i);
-		printf("%s", *words+i);
+		*(words+i)= (char *) malloc(sizeof(char *) * MAXWORD);
+		scanf("%s", *(words+i));
+//		printf("%s", *(words+i));
 	}
 
 	printf("Here's the words:\n");
 	for(int i=0; i<nwords; i++){
-		printf("%s", *(words+i));
+		printf("%s\n", *(words+i));
 	}
 
 
