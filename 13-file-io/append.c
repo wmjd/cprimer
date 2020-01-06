fbf41c46efaf30c1ad8189e0a19c275b10e517a3
William Diebolt <wmdiebolt@gmail.com>
2020-01-06 12:30:39 -0800

    append program works

--- 13-file-io/append.c
+++ 13-file-io/append.c
@@ -28,7 +28,7 @@ int main(void){
 		if(strcmp(file_src, file_app) == 0)
 			fputs("Can't append file to itself\n", stderr);
 		else if((fs=fopen(file_src, "r")) == NULL)
-			fprint(stderr, "Can't open %s", file_src);
+			fprintf(stderr, "Can't open %s", file_src);
 		else{
 			if(setvbuf(fs, NULL, _IOFBF, BUFSIZE) != 0){
 				fputs("Can't create input buffer\n", stderr);
@@ -41,7 +41,7 @@ int main(void){
 				fprintf(stderr, "Error in writing file %s.\n", file_app);
 			fclose(fs);
 			files++;
-			printf("File % appended.\n", file_src);
+			printf("File %s appended.\n", file_src);
 			puts("Next file (empty line to quit):");
 		}	
 	}
@@ -56,3 +56,23 @@ int main(void){
 	return 0;
 }
 
+void append(FILE * src, FILE * dst){
+	size_t bytes;
+	static char temp[BUFSIZE];
+	while((bytes=fread(temp,sizeof(char),BUFSIZE,src))>0)
+		fwrite(temp,sizeof(char),bytes,dst);
+}
+
+char * s_gets(char * st, int n){
+	char * ret_val;
+	char * find;
+	ret_val = fgets(st, n, stdin);
+	if(ret_val){
+		find = strchr(st, '\n');
+		if(find)
+			*find = '\0';
+		else
+			while(getchar() != '\n') continue;
+	}
+	return ret_val;
+}
