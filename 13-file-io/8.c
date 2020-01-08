c16fa38e3f725b1880de3c6130ed89bf087ea82f
William Diebolt <wmdiebolt@gmail.com>
2020-01-08 00:18:15 -0800

    char count of argv files

--- 13-file-io/8.c
+++ 13-file-io/8.c
@@ -9,23 +9,34 @@ int main(int argc, char ** argv){
 	
 	int c;
 	int count;
+	FILE * stream;
 
 	if(argc < 2){
-		printf("usage: cmd char [file1 file2 ...]\n");
+		fprintf(stderr, "usage: cmd char [file1 file2 ...]\n");
+		exit(EXIT_FAILURE);
 	}
 
 	if(argc == 2){
-		while((c=getchar()) != EOF){
+		while((c=getchar()) != EOF)
 			if(c == argv[1][0])
 				count++;
-		}
 		printf("The character: %c appears %d time(s) in stdin\n",
 			argv[1][0], count);
-	} else {
+		exit(EXIT_SUCCESS);
+	} 
 
-	}
+	for(int i=2; i < argc; i++){
+		if((stream = fopen(argv[i], "r")) == NULL){
+			fprintf(stderr, "can't open file: %s\n", argv[i]);
+			exit(EXIT_FAILURE);
+		}
+		while((c=fgetc(stream)) != EOF)
+			if(c == argv[1][0])
+				count++;
+		printf("The character: %c appears %d time(s) in %s\n",
+			argv[1][0], count, argv[i]);
+		count = 0;
+		}
 
-	
-
-	return 0;
+	exit(EXIT_SUCCESS);	
 }
