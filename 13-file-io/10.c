bf831a5293af277df3cca1eb26ec11c0a4448767
William Diebolt <wmdiebolt@gmail.com>
2020-01-09 13:40:51 -0800

    corner cases TODO

--- 13-file-io/10.c
+++ 13-file-io/10.c
@@ -6,4 +6,36 @@ loop: user supplies: pos
 		negative pos to end
 */
 
+#include <stdio.h>
 
+int main(void){
+	char filename[128];
+	FILE * stream;
+	long int pos;
+	int ch;
+	int end;
+
+	printf("Enter a file: ");
+	scanf("%s", filename);
+	if((stream = fopen(filename, "r")) == NULL){
+		printf("Can't open %s. Bye-bye", filename);
+		return 1;
+	}
+
+	fseek(stream, 0L, SEEK_END);
+	end = ftell(stream);
+
+	do{
+		printf("Enter a position: ");
+		scanf("%ld", &pos);
+		if(pos > end){
+			puts("That is not a valid position.");
+			continue;
+		}
+		fseek(stream, pos, SEEK_SET); 
+		while((ch=getc(stream)) != '\n')
+			putchar(ch);
+		putchar('\n');
+	}while(pos >= 0);
+	
+}
